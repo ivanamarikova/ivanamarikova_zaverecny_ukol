a4cf9627907325511b2c80429d42335985b78118
ivana.marikova <marikova.ivana@gmail.com>
2023-08-02 14:15:14 +0200

    ddt create project task

---
+++ cypress/e2e/pmtool/ddt_create_task_project_test.cy.js
@@ -0,0 +1,47 @@
+import { LoginPage } from "../../page-objects/pmtool/login_page";
+import { NewTaskWindowPage } from "../../page-objects/pmtool/new_task_window_page";
+import { ProjectsPage } from "../../page-objects/pmtool/projects_page";
+import { ProjectsTasksPage } from "../../page-objects/pmtool/projects_tasks_page";
+import { faker } from "@faker-js/faker";
+
+import newProjectTaskDdt from "../../fixtures/new_project_task_ddt.json";
+import { MenuSection } from "../../page-objects/pmtool/menu_sections";
+
+describe("Data Driven Tests create project tasks", () => {
+    let newTaskWindowPage;
+
+    beforeEach(() => {
+        new LoginPage()
+            .openPmtool()
+            .typeAdminUserName()
+            .typeAdminPassword()
+            .clickLogin();
+
+        new MenuSection()
+            .clickProjects();
+
+        new ProjectsPage()
+            .openFirstProjectDetail();
+
+        new ProjectsTasksPage()
+            .clickAddTask();
+
+        newTaskWindowPage = new NewTaskWindowPage();
+    });
+
+    newProjectTaskDdt.forEach((task) => {
+        it(`DDT task: ${task.Name_prefix}`, () => {
+            const task_name = `${task.Name_prefix}${faker.number.int({
+                max: 99999,
+            })}`;
+
+            newTaskWindowPage
+                .typeTaskName(task_name)
+                .selectTaskType(task.Type)
+                .selectTaskStatus(task.Status)
+                .selectTaskPriority(task.Priority)
+
+            newTaskWindowPage.waitForSaveCompletion();
+        });
+    });
+});
